90112cf0d251d98464ba5e6462fe342fcc591021
Alex MacArthur <alex@macarthur.me>
2024-03-17 22:00:53 -0500

    Add middleware tests.

---
+++ tests/Unit/Middleware/TransformHtmlTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace PicPerf\StatamicPicPerf;
+
+use PicPerf\StatamicPicPerf\Middleware\TransformHtml;
+
+it('returns early when it is a GET request', function () {
+    $partialMock = $this->createPartialMock(TransformHtml::class, ['getConfig', 'transformMarkup']);
+
+    $request = new \Illuminate\Http\Request();
+    $request->setMethod('GET');
+    $response = new \Illuminate\Http\Response();
+    $response->headers->set('content-type', 'text/html');
+
+    $next = function () use ($response) {
+        return $response;
+    };
+
+    $partialMock->expects($this->never())->method('transformMarkup');
+
+    $result = $partialMock->handle($request, $next);
+
+    expect($result)->toBe($response);
+});
+
+it('returns early when it is not a HTML response', function () {
+    $partialMock = $this->createPartialMock(TransformHtml::class, ['getConfig', 'transformMarkup']);
+
+    $request = new \Illuminate\Http\Request();
+    $request->setMethod('POST');
+    $response = new \Illuminate\Http\Response();
+    $response->headers->set('content-type', 'application/json');
+
+    $next = function () use ($response) {
+        return $response;
+    };
+
+    $partialMock->expects($this->never())->method('transformMarkup');
+
+    $result = $partialMock->handle($request, $next);
+
+    expect($result)->toBe($response);
+});
+
+it('returns early when config option is disabled', function () {
+    $partialMock = $this->createPartialMock(TransformHtml::class, ['getConfig', 'transformMarkup']);
+
+    $request = new \Illuminate\Http\Request();
+    $request->setMethod('GET');
+    $response = new \Illuminate\Http\Response();
+    $response->headers->set('content-type', 'text/html');
+
+    $next = function () use ($response) {
+        return $response;
+    };
+
+    $partialMock->method('getConfig')->willReturn(false);
+
+    $partialMock->expects($this->never())->method('transformMarkup');
+
+    $result = $partialMock->handle($request, $next);
+
+    expect($result)->toBe($response);
+});
+
+it('transforms the HTML', function () {
+    $partialMock = $this->createPartialMock(TransformHtml::class, ['getConfig', 'transformMarkup']);
+
+    $request = new \Illuminate\Http\Request();
+    $request->setMethod('GET');
+    $response = new \Illuminate\Http\Response();
+    $response->headers->set('content-type', 'text/html');
+    $response->setContent('<img src="https://example.com/image.jpg">');
+
+    $next = function () use ($response) {
+        return $response;
+    };
+
+    $partialMock->method('getConfig')->willReturn(true);
+    $partialMock->method('transformMarkup')->willReturn('<img src="https://picperf.io/https://example.com/image.jpg">');
+
+    $result = $partialMock->handle($request, $next);
+
+    expect($result->getContent())->toBe('<img src="https://picperf.io/https://example.com/image.jpg">');
+});
